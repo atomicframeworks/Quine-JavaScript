f618f3800f6f64337a91e7875eb6071543653f29
Kevin McGinty <atomic.frameworks@gmail.com>
2021-09-05 11:49:25 -0400

    Update quine.js

--- quine.js
+++ quine.js
@@ -1,4 +1,4 @@
-// The main quine function is stored here as array of string characters
+// The main quine function (copy of below) is stored here as array of string characters
 var charData = [
 	'(', 'f', 'u', 'n', 'c', 't', 'i', 'o', 'n', ' ', 'q', 'u', 'i', 'n', 'e', ' ', '(', ')', ' ', '{',
 		'v', 'a', 'r', ' ', 'r', 'e', 'c', 'r', 'e', 'a', 't', 'e', 'C', 'h', 'a', 'r', 'D', 'a', 't', 'a', 'C', 'o', 'd', 'e', ' ', '=', ' ',  '\'', 'v', 'a', 'r', ' ', 'c', 'h', 'a', 'r', 'D', 'a', 't', 'a', ' ', '=', ' ', '[', '\'', ';',
@@ -18,7 +18,7 @@ var charData = [
 
 (function quine () {
 	
-	// Recreate the charData variable as string
+	// Recreate the charData variable above as a new string
 	var recreateCharDataCode = 'var charData = [';
 	charData.forEach(function (char) {
 		// Account for (re)escaping characters
@@ -32,7 +32,6 @@ var charData = [
 	});
 	recreateCharDataCode = recreateCharDataCode + '];';
 	
-	// Add the recreate charData code to the code contained in the charData itself
-	
+	// Add the recreated charData variable (now represented as a string) to the code contained in the charData itself (which is thte quine function). Next output the quine via console.log()
 	console.log(recreateCharDataCode + charData.join(''));
 })();
